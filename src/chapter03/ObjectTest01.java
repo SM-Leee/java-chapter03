11fde9ff01e7b64b7f6818d5b0940068fc1827ce
BIT <BIT@DESKTOP-E23PFRN>
2019-01-03 10:26:05 +0900

    190103 10:25 Object method 기본구현

--- src/chapter03/ObjectTest01.java
+++ src/chapter03/ObjectTest01.java
@@ -1,5 +1,17 @@
 package chapter03;
 
 public class ObjectTest01 {
+	
+	public static void main(String[] arg) {
+		
+		Point p = new Point(10, 20);
+		System.out.println(p.getClass());	// reflection ( runtime할때 알려준다 ) 
+		System.out.println(p.hashCode());	// reference value x ( 레퍼런스 변수에 저장되어 있다)
+											// address x
+											// address의 기반의 hashing 값을 만들어냄(address 기방의 hashing은 유일하게 하나이다)
+		
+		System.out.println(p);
+		System.out.println(p.toString());	// getClass() + "@" + hashCode()-16진법
+	}
 
 }
